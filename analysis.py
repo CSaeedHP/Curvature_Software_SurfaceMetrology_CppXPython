e701d46f8de6d792c57ac88df9b7b97a1a966f85
Jaganmranal <93226599+Jaganmranal@users.noreply.github.com>
2024-02-16 18:26:11 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ from tests import *
 
 #this processes all the curvatures
 
+#returns the curvature of a given 2d array of 3 points
 def curvature(points):
     if obtuse.isObtuse(points):
         return herons.herons_calc(points) * (2 * sign.sign_calc(points) - 1)
@@ -10,6 +11,7 @@ def curvature(points):
         return calculus.quad_curvature(points) * (2 * sign.sign_calc(points) - 1)
     
 #data is a two dimensional list of points
+#points_org is a 4-d list (1st level: scales; 2nd level: set of 3 points (this will later be converted into curvatures); 3rd level: x,y coordinates of a single point)
 def organize_data(data):
     points_org = []
     n = len(data)
@@ -24,7 +26,7 @@ def organize_data(data):
         scale += 1
     return points_org
 
-#collects the data from sineP.txt
+#collects the data from sineP.txt and returns a list of points (2d list)
 def format_data(file):
     my_file = open(file, "r") 
     data = my_file.read().split()
@@ -34,8 +36,10 @@ def format_data(file):
         data[i] = [x, y]
     for x in data:
         return data
-    
+
+
 def get_curvature(data):
+    #positions is a list of lists, which each sublist represent a scale and each individual element is the central x-value for that point 
     positions = []
     #print(positions)
     for scale in range(len(data)):
@@ -53,10 +57,11 @@ def compare(data, real):
         sum += abs(real[i+1][1] - data[i])/real[i][1]
     return 100 * sum/len(real)
 
+#gets the 'pixel length' of the data
 def get_spacing():
     data = format_data("sineP.txt")
     return data[1][0] - data[0][0]
 
-
+#returns the actual curvature data for the test sample
 def get_actual():
     return format_data("sineC.txt")
