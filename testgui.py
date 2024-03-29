e81cc35e67bf0518a6f10f98462f350c72d52779
LiquidPeanuts <jchang9360@gmail.com>
2024-03-29 17:53:18 -0400

    gui update

--- testgui.py
+++ testgui.py
@@ -2,8 +2,10 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 import os
+import csv
 
 root = Tk()
+root.title("curvature project development")
 
 filename = StringVar()
 
@@ -26,6 +28,7 @@ def filelabeling():
     if file:
         filename.set(os.path.abspath(file.name))
         fileobject.set(file)
+
         return
     filename.set("No file selected")
     fileobject.set(False)
@@ -42,7 +45,7 @@ checkfilebutton.pack()
 
 
 
-#start post working1 modifications
+#start post working1 modifications, dropdown function options
 
 function_keys = {
     "herons" : "herons_curvature",
@@ -65,5 +68,38 @@ FunctionCombo.bind("<<ComboboxSelected>>", FunctionKeySelect)
 FunctionCombo.pack()
 
 
+# working 2
 
+def DataReader(my_file):
+    data = my_file.read().split()
+    for i in range(len(data)):
+        point = data[i].split(',')
+        x = float(point[0]); y = float(point[1])
+        data[i] = [x, y]
+    for x in data:
+        return data
+
+entrynumber = StringVar()
+minimumbound = Label(root, text = "insert minimum bound below")
+minimumentry = Entry(root, textvariable = entrynumber, width = 50, borderwidth = 1)
+entrynumber.trace_add("write", )
+
+
+minimumbound.pack()
+minimumentry.pack()
+
+
+sv = StringVar()
+sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
+e = Entry(root, textvariable=sv)
+e.pack()
+
+def checkbound():
+    print(type(minimumentry.get()))
+
+boundbutton = Button(root, text = "check bound type", command = checkbound)
+boundbutton.pack()
+# e = Entry(root, width = 50,borderwidth = 100)
+# e.pack()
+# e.insert(0,"default value")
 root.mainloop()
